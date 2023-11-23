2b2e4034f49839cd972829115932fa335b3ec7d2
NicolasM-83200 <manigand.nicolas@gmail.com>
2023-11-23 09:52:19 +0100

    ajout du composant Collapse.jsx et de son style

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef, useState } from "react";
+import arrowUp from "../assets/img/arrow_up.png";
+
+const Collapse = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const textRef = useRef(null);
+
+  const toogleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const contentHeight = textRef.current.scrollHeight;
+    textRef.current.style.height = isOpen ? `${contentHeight}px` : "0px";
+  }, [isOpen]);
+
+  return (
+    <div className={`collapse-item ${props.className}`}>
+      <div className="collapse-item-title">
+        <span>{props.title}</span>
+        <img
+          className={`arrow ${isOpen ? "open" : ""}`}
+          src={arrowUp}
+          alt="Flèche pour déplier item"
+          onClick={toogleCollapse}
+        />
+      </div>
+      <div
+        className={`collapse-item-text-container ${isOpen ? "open" : "closed"}`}
+        ref={textRef}
+      >
+        <div className="collapse-item-text">{props.text}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Collapse;
